090564df35a59138788df0d68b14af8956c3e507
Katherine Lynch <kat3drx@gmail.com>
2014-08-05 10:09:06 -0400

    Added Display datastream and test

---
+++ lib/tul_ohist/datastreams/display_datastream.rb
@@ -0,0 +1,20 @@
+module TulOhist
+
+  module Datastreams
+  
+    class DisplayDatastream < ActiveFedora::OmDatastream
+
+      set_terminology do |t|
+        t.root(path: "fields")
+        t.weight index_as: :displayable #Defining order of display for collection objects on Oral History page
+      end 
+
+      def self.xml_template
+        Nokogiri::XML.parse("<fields/>")
+      end
+    
+    end
+
+  end
+end
+
