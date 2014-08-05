099ea3fb111a8a03d328006bd495c10938860d23
Steven Ng <steven.ng@temple.edu>
2014-08-05 17:00:24 -0400

    Add label field

--- lib/tul_ohist/models/base.rb
+++ lib/tul_ohist/models/base.rb
@@ -1,13 +1,13 @@
 module TulOhist::Models
   class Base < ActiveFedora::Base
-    has_metadata "contentdm", type: TulOhist::Datastreams::ContentdmDatastream
-    has_metadata "base", type: TulOhist::Datastreams::BaseDatastream
-    has_metadata "geographic", type: TulOhist::Datastreams::GeographicDatastream
-    has_metadata "notations", type: TulOhist::Datastreams::NotationsDatastream
-    has_metadata "physical", type: TulOhist::Datastreams::PhysicalDatastream
-    has_metadata "rights", type: TulOhist::Datastreams::RightsDatastream
-    has_metadata "related_items", type: TulOhist::Datastreams::RelatedItemsDatastream
-    has_metadata "id", type: TulOhist::Datastreams::IdDatastream
-    has_metadata "creation", type: TulOhist::Datastreams::CreationDatastream
+    has_metadata "contentdm", label: "CDM metadata", type: TulOhist::Datastreams::ContentdmDatastream
+    has_metadata "base", label: "Base metadata", type: TulOhist::Datastreams::BaseDatastream
+    has_metadata "geographic", label: "Geographic metadata", type: TulOhist::Datastreams::GeographicDatastream
+    has_metadata "notations", label: "Notation metadata", type: TulOhist::Datastreams::NotationsDatastream
+    has_metadata "physical", label: "Physical metadata", type: TulOhist::Datastreams::PhysicalDatastream
+    has_metadata "rights", label: "Rights metadata",  type: TulOhist::Datastreams::RightsDatastream
+    has_metadata "related_items", label: "Related items metadata", type: TulOhist::Datastreams::RelatedItemsDatastream
+    has_metadata "id", label: "ID metadata", type: TulOhist::Datastreams::IdDatastream
+    has_metadata "creation", label: "Creation metadata", type: TulOhist::Datastreams::CreationDatastream
   end
 end
