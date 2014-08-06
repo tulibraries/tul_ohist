bd3592c2d0dfb3f145e1f396b65c23819ebf4890
Katherine Lynch <kat3drx@gmail.com>
2014-08-06 10:28:17 -0400

    Updated ContentDM indexing methods

--- lib/tul_ohist/datastreams/contentdm_datastream.rb
+++ lib/tul_ohist/datastreams/contentdm_datastream.rb
@@ -6,14 +6,14 @@ module TulOhist
 
       set_terminology do |t|
         t.root(path: "fields")
-        t.item_url index_as: :stored_searchable
-        t.oclc_number index_as: :stored_searchable
-        t.date_created index_as: :stored_searchable
-        t.date_modified index_as: :stored_searchable
-        t.contentdm_number index_as: :stored_searchable
-        t.contentdm_file_name index_as: :stored_searchable
-        t.contentdm_file_path index_as: :stored_searchable
-        t.contentdm_collection_id index_as: :stored_searchable
+        t.item_url index_as: :displayable
+        t.oclc_number index_as: :displayable
+        t.date_created index_as: :displayable
+        t.date_modified index_as: :displayable
+        t.contentdm_number index_as: :displayable
+        t.contentdm_file_name index_as: :displayable
+        t.contentdm_file_path index_as: :displayable
+        t.contentdm_collection_id index_as: :displayable
       end 
 
       def self.xml_template
