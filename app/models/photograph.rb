3f34db568f50b1c12edd62cf373243a804aec8e9
Steven Ng <steven.ng@temple.edu>
2014-08-06 16:50:46 -0400

    Changes portrait class to photograph class

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -1,4 +1,4 @@
-class Portrait < TulOhist::Models::Base
+class Photograph < TulOhist::Models::Base
 	has_metadata "base", label: "Base metadata", type: TulOhist::Datastreams::BaseDatastream
 	has_metadata "digital", label: "Digital metadata", type: TulOhist::Datastreams::DigitalDatastream
 end
--- spec/models/photograph_spec.rb
+++ spec/models/photograph_spec.rb
@@ -1,5 +1,5 @@
-describe 'Portrait' do
-  subject { Portrait.new }
+describe 'Photograph' do
+  subject { Photograph.new }
 
   it { is_expected.to have_datastream_of_type("base", TulOhist::Datastreams::BaseDatastream) }
   it { is_expected.to have_datastream_of_type("digital", TulOhist::Datastreams::DigitalDatastream) }
