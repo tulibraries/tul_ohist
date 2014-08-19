0f5a4e315f2ca67001cad3c3892f08b2f767aa61
Katherine Lynch <kat3drx@gmail.com>
2014-08-19 15:05:07 -0400

    Fixed circular dependency (hallelujah) added to compound object show and controller

--- app/controllers/compound_objects_controller.rb
+++ app/controllers/compound_objects_controller.rb
@@ -1,7 +1,7 @@
 class CompoundObjectsController < ApplicationController
   include Hydra::Controller::ControllerBehavior
   
-  helper_method :render_title, :render_blurb, :render_image_grid
+  helper_method :render_title, :render_objects
   
   def index
   end
@@ -13,6 +13,37 @@ class CompoundObjectsController < ApplicationController
   def render_title(master_identifier) 
     output = "<h1>Welcome to #{master_identifier}</h1>"
     return output.html_safe
+  end
+
+  def render_objects(master_identifier)
+    b = ActiveFedora::Base.where(master_identifier_ssim: master_identifier).to_a
+    
+    titles = Array.new    
+    dates = Array.new    
+    
+    b.each do |b_obj|
+      pid=b_obj.id
+      pid_fragments = pid.split(":");
+      content_model = pid_fragments.first;
+      case content_model
+        when 'photograph' then object = Photograph.find(pid)
+        when 'transcript' then object = Transcript.find(pid)
+        else nil
+      end
+      
+      if(object)
+       intermed_title = object.title.first
+       (titles ||= []) << intermed_title
+
+       intermed_date = object.date.first
+       (dates ||= []) << intermed_date
+      end
+    end
+    # puts "argh"
+    # puts title
+    # binding.pry
+    return titles
+
   end
   
   
