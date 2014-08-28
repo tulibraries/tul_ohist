e53ee065f839a9a6311e1766eb1cd62433efac5b
Katherine Lynch <kat3drx@gmail.com>
2014-08-28 10:02:09 -0400

    Normalized capitalization of variables

--- app/helpers/blacklight/url_helper_behavior.rb
+++ app/helpers/blacklight/url_helper_behavior.rb
@@ -3,10 +3,10 @@ module Blacklight::UrlHelperBehavior
 	extend Deprecation
 	self.deprecation_horizon = 'blacklight 6.0'
 
-	def link_to_document(doc, opts={:label=>nil, :counter => nil, :linkClass => nil})
+	def link_to_document(doc, opts={:label=>nil, :counter => nil, :link_class => nil})
 		opts[:label] ||= document_show_link_field(doc)
 		label = render_document_index_label doc, opts
-		link_to label, url_for_document(doc), :class => opts[:linkClass] 
+		link_to label, url_for_document(doc), :class => opts[:link_class] 
 		# link_to label, url_for_document(doc), document_link_params(doc, opts)
 	end
 
