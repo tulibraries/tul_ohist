ff89aa38171852269d5ca06419a6272c8ddb2181
Katherine Lynch <kat3drx@gmail.com>
2014-08-29 10:02:42 -0400

    Click-through on search results and transcript partials functioning, stores click-through-terms agreed to in cookie

--- app/assets/javascripts/click_through.js
+++ app/assets/javascripts/click_through.js
@@ -1,33 +1,30 @@
 $(document).ready(function(){
   
   $(".clickthroughConfirm").click(function(){
-    $.cookie('click_through', 'accepted');
-    alert("Cookie: " + $.cookie('click_through'));
+  	var clickThroughText = $(".modal-dialog .modal-body").text();
+    $.cookie('click_through', $(".modal-dialog .modal-body").text());
+    addAcceptedItem(clickThroughText);
   });
 
 });
 
-function checkForConfirmation(clickThroughTerms, confirmationButton, cancellationButton, preamble){ 
-  preamble = preamble || '';
+function checkForConfirmation(clickThroughTerms, confirmationButton, cancellationButton){ 
   $transcriptDiv = $('#transcript-pdf');
   if ($transcriptDiv.length){
     //To remove a cookie:
     //$.removeCookie('click_through', { path: '/' });
     var $cookieVal = $.cookie('click_through');
-    if(preamble){
-      var $preamble_wrapped = "<div class=\"preamble\">" + preamble + "</div>";
-      clickThroughTerms = $preamble_wrapped + clickThroughTerms;
-    }
 
-    if($cookieVal != 'accepted'){
-        
-        hideUnacceptedContent();
-
+    acceptedTerms = $.cookie('acceptedTerms');
+    if(!acceptedTerms || (acceptedTerms.indexOf(clickThroughTerms) == -1)){
+    	hideUnacceptedContent();
 		$.confirm({
 		    text: clickThroughTerms,
 		    confirm: function(button) {
-		      $.cookie('click_through', 'accepted');
+		      $.cookie('click_through', clickThroughTerms);
 		      displayAcceptedContent();
+		      addAcceptedItem(clickThroughTerms);
+
 		    },
 		    cancel: function(button) {
 		      hideUnacceptedContent();      
@@ -46,10 +43,35 @@ function checkForConfirmation(clickThroughTerms, confirmationButton, cancellatio
     $('#main-container').show();
  }
 
- function hideUnacceptedContent(overlay){
+ function hideUnacceptedContent(){
  	var overlay = jQuery('<div id="overlay"> </div>');
  	if($("#overlay").length == 0){
       overlay.appendTo(document.body);
     }
    $('#main-container').hide();
+ }
+
+
+ function addAcceptedItem(newItem){
+ 	var itemsArray = new Array();	
+ 	var acceptedTerms = $.cookie('acceptedTerms');
+    if(!acceptedTerms){
+      $.cookie('acceptedTerms', newItem);
+    } else {
+      itemsArray = acceptedTerms.split(',');
+      if(acceptedTerms.indexOf(newItem) == -1){
+	    itemsArray.push(newItem);
+	    $.cookie('acceptedTerms', itemsArray.join(','));
+	  }
+
+    }
+
+ }
+
+function searchResultsClickThroughConfirmBox(clickThroughTerms, confirmationButton, cancellationButton){
+	$(".clickthroughConfirm").confirm({
+	    text: clickThroughTerms,
+	    confirmButton: confirmationButton,
+	    cancelButton: cancellationButton,
+	});
  }
