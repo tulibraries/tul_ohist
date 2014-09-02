8dbd2d203c01acf8fb01c0a16b7897003758d244
Katherine Lynch <kat3drx@gmail.com>
2014-09-02 16:57:08 -0400

    Fixed glyphicons issue, added SASS CSS, added more custom theming

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '~> 4.1.4'
 gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -5,4 +5,10 @@ module BlacklightHelper
 	# 	label = render_document_index_label doc, opts
 	# 	link_to label, document_or_object_url(doc)
 	# end
+
+	def link_to_document(doc, opts={:label=>nil, :counter => nil, :link_class => nil})
+		opts[:label] ||= document_show_link_field(doc)
+		label = render_document_index_label doc, opts
+		link_to label, url_for_document(doc), :class => opts[:link_class] 
+	end
 end
