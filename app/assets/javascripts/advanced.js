3f3a8de1adab0c76c8489f7abaf67c853a7eeb7c
Steven Ng <steven.ng@temple.edu>
2016-04-12 17:02:35 -0400

    Integrate digital collections into form

---
+++ app/assets/javascripts/advanced.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $( ".advanced-search-form .panel-heading" ).removeClass( "collapsed collapse-toggle" );
+  $( ".advanced-search-form #facet-digital_collection_sim" ).removeClass( "panel-collapse facet-content collapse" );
+})
