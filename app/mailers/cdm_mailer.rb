baf9c640348aa1e9703b75e66f9c31814091ecde
Steven Ng <steven.ng@temple.edu>
2016-06-14 14:03:06 -0400

    Fixes and cleans up log file usage
    - Always creates a log file
    - Makes log names configurable
    
    Use non-Rails dependent path to config and log
    
    Localize text and add tests
    
    - Add basic test for mail content
    - Localize mail subject and tests
    - Replace app parameter file with sample
    
    Fix attachement expectation
    
    Break out each expectation

--- app/mailers/cdm_mailer.rb
+++ app/mailers/cdm_mailer.rb
@@ -3,17 +3,26 @@ class CdmMailer < ApplicationMailer
   default from: "No-Reply LibDigital <no-reply-libdigital@temple.edu>"
 
   def report_download_errors
-    attachments['cron_errors.log'] = File.read('log/cron_error_log.log')
-    mail(:to => "tuf73699@temple.edu,tug34268@temple.edu", :subject => "ERROR: CONTENTdm download for TULOHIST error", :body => "Something went wrong during the CONTENTdm nightly download.  Please see attached error log.")
+    tul_ohist_config = YAML.load_file(File.expand_path(File.join('config', 'tul_ohist.yml')))
+    attachments[tul_ohist_config['cron_error_log']] = File.read(File.join('log', tul_ohist_config['cron_error_log']))
+    mail(to:      tul_ohist_config['email_recipient'],
+         subject: t('tul_ohist.mailer.nightly_download_error.subject'),
+         body:    t('tul_ohist.mailer.nightly_download_error.body'))
   end
 
   def report_ingest_errors
-    attachments['ingest.log'] = File.read('log/ingest.log')
-    mail(:to => "tuf73699@temple.edu,tug34268@temple.edu", :subject => "ERROR: CONTENTdm ingest for TULOHIST error", :body => "Something went wrong during the CONTENTdm nightly download.  Please see attached error log.")
+    tul_ohist_config = YAML.load_file(File.expand_path(File.join('config', 'tul_ohist.yml')))
+    attachments[tul_ohist_config['ingest_log']] = File.read(File.join('log', tul_ohist_config['ingest_log']))
+    mail(to:      tul_ohist_config['email_recipient'],
+         subject: t('tul_ohist.mailer.ingest_error.subject'),
+         body:    t('tul_ohist.mailer.ingest_error.body'))
   end
 
   def report_success
-    attachments['cron_log.txt'] = File.read('log/cron_log.log')
-    mail(:to => "tuf73699@temple.edu,tug34268@temple.edu", :subject => "SUCCESS: CONTENTdm backup and reindex for TULOHIST", :body => "Successful backup and reingest of TULOHIST collection items. Please see attached log for more information.")
+    tul_ohist_config = YAML.load_file(File.expand_path(File.join('config', 'tul_ohist.yml')))
+    attachments[tul_ohist_config['cron_log']] = File.read(File.join('log', tul_ohist_config['cron_log']))
+    mail(to:      "tuf73699@temple.edu,tug34268@temple.edu",
+         subject: t('tul_ohist.mailer.backup_log.subject'),
+         body:    t('tul_ohist.mailer.backup_log.body'))
   end
 end
