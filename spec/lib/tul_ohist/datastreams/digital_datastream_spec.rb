b60c6593bccf21a7aff1e4a41f6beb512d1b8db6
Steven Ng <steven.ng@temple.edu>
2016-06-24 17:30:28 -0400

    Add support for multiple ensemble players.
    Ensemble_identifier is multivalued
    
    Embed player without iframe
    
    Test player script has Ensemble identifier
    
    - Test for plugin call has the Ensemble ID
    - Pass the Ensemble ID into the player renderer
    
    Fixes non-rendering player
    
    - Disables turbolink.

--- spec/lib/tul_ohist/datastreams/digital_datastream_spec.rb
+++ spec/lib/tul_ohist/datastreams/digital_datastream_spec.rb
@@ -1,22 +1,14 @@
 describe "DigitalDatastream" do
 
+  let (:datastream_file) do
+     File.join(Rails.root, 'spec', 'samples', 'digital_datastream.xml')
+  end
+
   subject do
     datastream = TulOhist::Datastreams::DigitalDatastream.new
-    datastream.content = <<EODS
-<foxml:datastream ID="digitalMetadata" STATE="A" CONTROL_GROUP="X" VERSIONABLE="true">
-  <foxml:datastreamVersion ID="digitalMetadata.0" LABEL="digital metadata" MIMETYPE="text/xml">
-    <foxml:xmlContent>
-      <fields>
-        <digitization_specifications>MPEG 1/2 layer III</digitization_specifications>
-        <original_recording_specifications>mode 2, 32 kHz/16 bits, linear</original_recording_specifications>
-        <transcript_filename>UA0001.pdf</transcript_filename>
-        <audio_filename>UA0001.mp3</audio_filename>
-        <photograph_filename>UA0001.tif</photograph_filename>
-      </fields>
-    </foxml:xmlContent>
-  </foxml:datastreamVersion>
-</foxml:datastream>
-EODS
+    File.open( datastream_file ) do |f|
+      datastream.content = f.read
+    end
     datastream
   end
 
@@ -25,5 +17,6 @@ EODS
   it { is_expected.to have_term(:transcript_filename) }
   it { is_expected.to have_term(:audio_filename) }
   it { is_expected.to have_term(:photograph_filename) }
+  it { is_expected.to have_term(:ensemble_identifier) }
 
 end
